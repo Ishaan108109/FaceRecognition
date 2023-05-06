bd5451c1f714a6ad3a9a958f74de46f2c1f87f6f
Ishaan <ishaan2077@gmail.com>
2023-05-06 16:39:52 +0530

    Uri to Base 64

--- app/src/main/java/com/example/cameraxintegration/Utils.kt
+++ app/src/main/java/com/example/cameraxintegration/Utils.kt
@@ -3,6 +3,7 @@ package com.example.cameraxintegration
 import android.content.ContentResolver
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Base64
@@ -14,37 +15,23 @@ import java.io.IOException
 
 object Utils {
     fun convertUriToBase64(context: Context, uri: Uri): String? {
-        var inputStream: java.io.InputStream? = null
-        try {
-            val scheme = uri.scheme
-            if (scheme == "file") {
-                val file = File(uri.path)
-                inputStream = FileInputStream(file)
-            } else {
-                val contentResolver: ContentResolver = context.contentResolver
-                inputStream = contentResolver.openInputStream(uri)
-            }
-            if (inputStream == null) {
-                return null
-            }
-            val buffer = ByteArray(8192)
-            val output = ByteArrayOutputStream()
-            var bytesRead = inputStream.read(buffer)
-            while (bytesRead != -1) {
-                output.write(buffer, 0, bytesRead)
-                bytesRead = inputStream.read(buffer)
-            }
-            val imageBytes = output.toByteArray()
-            val base64 = Base64.encodeToString(imageBytes, Base64.NO_WRAP)
-            return base64
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-            return null
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return null
-        } finally {
-            inputStream?.close()
-        }
+        val loadedImage = MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+        val bitmap = Bitmap.createScaledBitmap(loadedImage!!, 400, 300, true)
+
+        val rotatedBitmap = bitmap.rotate(-90f)
+
+        val compressBitmapPrev: Bitmap? = rotatedBitmap //compressImage1(bitmapPrev, 100.0)
+        val out = ByteArrayOutputStream()
+        compressBitmapPrev?.compress(Bitmap.CompressFormat.PNG, 0, out)
+        val byteArray = out.toByteArray()
+        val flags = android.util.Base64.NO_WRAP or android.util.Base64.DEFAULT
+        return Base64.encodeToString(byteArray, flags)
+
+
+    }
+
+    fun Bitmap.rotate(degrees: Float): Bitmap {
+        val matrix = Matrix().apply { postRotate(degrees) }
+        return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
     }
 }
